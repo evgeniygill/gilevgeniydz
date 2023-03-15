e01f64addcbd26d1b5294da2cf07b0cfba8edbcd
evgeniy <Evgeniygil@mail.ru>
2023-03-15 10:56:50 +0300

    new commit 08,09,10

---
+++ task008/Program.cs
@@ -0,0 +1,41 @@
+﻿// 1.Задайте массив заполненный случайными положительными трёхзначными числами.
+// 2.Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] ThreeDigitArrRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int EvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+int[] array = ThreeDigitArrRndInt(4, 100, 999);
+PrintArray(array);
+int evenNumbers = EvenNumbers(array);
+Console.WriteLine($"количетсво четных чисел -> {evenNumbers}");
---
+++ task10/Program.cs
@@ -0,0 +1,53 @@
+﻿// 1.Задайте массив вещественных чисел.
+// 2.Найдите разницу между максимальным и минимальным элементов массива.
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+double[] CreatArrayRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        double num = rnd.NextDouble() * (max - min) + min;
+        arr[i] = Math.Round(num, 1);
+    }
+    return arr;
+}
+void PrintArrayS(double[] arr)
+{
+    Console.Write($"[ ");
+    int count = arr.Length;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"] ");
+}
+double MaxElement(double[] arr)
+{
+    double max = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (max < arr[i]) max = arr[i];
+    }
+    Console.WriteLine($"Максимальный элемент {max}");
+    return max;
+}
+double MinElement(double[] arr)
+{
+    Console.Write($" ");
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (min > arr[i]) min = arr[i];
+    }
+    Console.WriteLine($"Минимальный элемент {min}");
+    return min;
+}
+
+double[] creatArrRnd = (CreatArrayRndDouble(5, 1, 100));
+PrintArrayS(creatArrRnd);
+double maxelem = MaxElement(creatArrRnd);
+double minelem = MinElement(creatArrRnd);
+Console.WriteLine($"Разность между максимальным элементом и минимальным: {maxelem - minelem}");
